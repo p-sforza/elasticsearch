49499fce3334b28acffd93743bed39e19ac5c8a3
Pierluigi Sforza <psforza@redhat.com>
2017-10-08 17:43:04 +0200

    Dockerfile change

--- insertdata.sh
+++ insertdata.sh
@@ -3,9 +3,8 @@
 runuser -l elasticsearch -c '/usr/share/elasticsearch/bin/elasticsearch -d'
 sleep 15
 chown elasticsearch:elasticsearch /earth_meteorite_landings.json
-netstat -anp | grep elastic
 
 es-json-load --data --file=/earth_meteorite_landings.json --index=testk --type=tipek;
 sleep 15;
-
+curl -vvvv -XPOST 'http://localhost:9200/_shutdown';
 
