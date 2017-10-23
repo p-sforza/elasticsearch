da58b00c75edf35dd8c06cbf10d7aede6330c12b
psforza@redhat.com <psforza@redhat.com>
2017-10-23 16:54:15 +0200

    <comment>

--- insertdata.sh
+++ insertdata.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 su --preserve-environment elasticsearch 
-
-#/home/elasticsearch/.bash_proflle
-
 env
 
-echo "!!!!!!!!!!!!" && env;
-
-runuser -l elasticsearch -c 'touch ${ELASTIC_LOG} && ls -lai /usr/share/elasticsearch/logs/';
-runuser -l elasticsearch -c 'echo "NEW BUILD FOR ELASTIC!" > "${ELASTIC_LOG}" ';
+touch ${ELASTIC_LOG} && ls -lai /usr/share/elasticsearch/logs/;
+echo "NEW BUILD FOR ELASTIC!" > "${ELASTIC_LOG}";
 
 
 ELASTIC_PID=$(ps -aux | grep -m1 elastic | grep -v grep | awk '{ print $2 }');
@@ -18,7 +13,8 @@ if [ $ELASTIC_PID ]; then
 else 
 	# START ELASTIC
 	echo "STARTING ELASTIC!";
-	runuser -l elasticsearch -c '/usr/share/elasticsearch/bin/elasticsearch -d'
+	/usr/share/elasticsearch/bin/elasticsearch -d
+
 	until [ STARTED=$(grep started "${ELASTIC_LOG}" ) ]; do
 		echo '   elastic not up...';
 		sleep 1;
@@ -27,12 +23,12 @@ else
 	echo "ELASTIC PROCESS: " && ps -aux | grep -m1 elastic;
 
 	# UPLOAD DATA
-	runuser -l elasticsearch -c 'echo "STARTING DATA UPLOAD!" >> "${ELASTIC_LOG}" ';
+	echo "STARTING DATA UPLOAD!" >> "${ELASTIC_LOG}" ;
 	chown elasticsearch:elasticsearch /earth_meteorite_landings.json
 	es-json-load --data --file=/earth_meteorite_landings.json --index=testk --type=tipek;
 	
 	# STOP ELASTIC
-	echo "SHUTTING DOWN ELASTIC!" && runuser -l elasticsearch -c 'echo "SHUTTING DOWN ELASTIC!" >> "{$ELASTIC_LOG}" ';
+	echo "SHUTTING DOWN ELASTIC!" && echo "SHUTTING DOWN ELASTIC!" >> "{$ELASTIC_LOG}" ;
 	ELASTIC_PID=$(ps -aux | grep -m1 elastic | grep -v grep | awk '{ print $2 }')
 	kill $ELASTIC_PID;
 	
